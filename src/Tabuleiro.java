a048b1f9d5cc0571ddd02720e06ad31cf06e10e7
EuSouAlaskax <57346466+EuSouAlaskax@users.noreply.github.com>
2020-10-01 15:10:51 -0300

    Update Tabuleiro.java

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -4,7 +4,7 @@ public class Tabuleiro {
 	int x = 6, y = 7;
 	int tab[][] = new int[x][y];
 	
-	public Tabuleiro() {
+	public Tabuleiro() { //no construtor preenchemos todo o tabuleiro 6x7 com "0" em cada "casa"
 		for (int linha = 0; linha < x; linha++) {
 			for (int coluna = 0; coluna < y; coluna++) {
 				tab[linha][coluna] = 0;
@@ -12,19 +12,19 @@ public class Tabuleiro {
 		}
 	}
 	
-	public void preencher(int posicaoX, int numero) {
+	public void preencher(int posicaoX, int numero) { //esse método encontra uma posição vazia
 		
-		int linhaAux = 0;
+		int linhaAux = 0; // a linha Auxiliar é para "escanear" a linha após a linha atual
 		
-		while (linhaAux < x && tab[linhaAux][posicaoX] == 0) {
+		while (linhaAux < x && tab[linhaAux][posicaoX] == 0) { //o while é utilizado para incrementar a linha auxilixar que verifica se a próxima linha está vazia ou não
 			linhaAux ++;
 		}
 		
-		if (linhaAux != 0 ) {
+		if (linhaAux != 0 ) { //se a próxima linha estiver vazia entao, um numero é printado nela, 1 ou 2 dependendo do jogador da vez
 		 tab [linhaAux -1][posicaoX] = numero;
 		}
 		else {
-			System.out.println("A coluna" +posicaoX+ "já está preenchida");
+			System.out.println("A coluna" +posicaoX+ "já está preenchida"); //essa mensagem é printada caso a coluna escolhida ja esteja cheia e nao hajam posições disponiveis
 		}
 	}
 	
@@ -47,21 +47,21 @@ public class Tabuleiro {
 	}
 	
 	@Override
-	public String toString() {
-		String str = " (0) (1) (2) (3) (4) (5) (6) \n";
+	public String toString() { //o toString aqui printa o tabuleiro bonitinho
+		String str = " (0) (1) (2) (3) (4) (5) (6) \n"; //linha fixa
 		for (int linha = 0; linha < x; linha++) {
-			str += "|---.---.---.---.---.---.---|\n";
+			str += "|---.---.---.---.---.---.---|\n"; //linhas fixas
 			for (int coluna = 0; coluna < y; coluna++) {
-				if(tab[linha][coluna] == 0) {
-					str += "|   "; // sao 3 espaçcos
+				if(tab[linha][coluna] == 0) { //o if é para quando o espaço tiver vazio
+					str += "|   ";
 				}
 				else {
-					str += "| " +tab[linha][coluna]+ " "; //perguntar pq n entra no else
+					str += "| " +tab[linha][coluna]+ " "; //o else é para quando tiver algum número printado (1 ou 2 no caso)
 				}
 			}
 			str += "|\n";
 		}
-		str += "|===.===.===.===.===.===.===|";
+		str += "|===.===.===.===.===.===.===|"; //linha fixa
 		return str;
 	}
 }
